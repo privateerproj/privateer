04c31c85e516be131ead1dd625ba91dff10b4653
Eddie Knight <knight@linux.com>
2023-02-09 04:24:03 -0600

    Added implicit raid selection from provided raid variables

--- internal/config/config.go
+++ internal/config/config.go
@@ -13,10 +13,11 @@ import (
 type varOptions struct {
 	VarsFile *string
 
-	AllRaids     *bool    `yaml:"AllRaids"`
-	Verbose      *bool    `yaml:"Verbose"`
-	BinariesPath string   `yaml:"BinariesPath"`
-	Run          []string `yaml:"Run"`
+	AllRaids     *bool                  `yaml:"AllRaids"`
+	Verbose      *bool                  `yaml:"Verbose"`
+	BinariesPath string                 `yaml:"BinariesPath"`
+	Run          []string               `yaml:"Run"`
+	Raids        map[string]interface{} `yaml:"Raids"`
 }
 
 // Vars is a stateful object containing the variables required to execute this pack
@@ -24,9 +25,8 @@ var Vars varOptions
 
 // Init will set values with the content retrieved from a filepath, env vars, or defaults
 func (ctx *varOptions) Init() (err error) {
-
 	if ctx.varsFileIsFound() {
-		sdkConfig.GlobalConfig.VarsFile = *ctx.VarsFile
+		sdkConfig.GlobalConfig.VarsFile = *ctx.VarsFile // TODO: is this line redundant?
 		ctx.decode()
 		if err != nil {
 			log.Printf("[ERROR] %v", err)
@@ -51,8 +51,7 @@ func (ctx *varOptions) varsFileIsFound() bool {
 }
 
 // decode uses an SDK helper to create a YAML file decoder,
-// parse the file to an object, then extracts the values from
-// Raid.Kubernetes into this context
+// parse the file to an object, then extracts the values into this context
 func (ctx *varOptions) decode() (err error) {
 	configDecoder, file, err := sdkConfig.NewConfigDecoder(*ctx.VarsFile)
 	if err != nil {
@@ -63,8 +62,17 @@ func (ctx *varOptions) decode() (err error) {
 	return err
 }
 
+func (ctx *varOptions) getRaids() (raids []string) {
+	for raid, _ := range ctx.Raids {
+		raids = append(raids, raid)
+	}
+	log.Printf("Raids: %s", raids)
+	return
+}
+
 func (ctx *varOptions) setEnvAndDefaults() {
 	setter.SetVar(&ctx.BinariesPath, "PRIVATEER_BIN", filepath.Join(sdkConfig.GlobalConfig.InstallDir, "bin"))
+	setter.SetVar(&ctx.Run, "PRIVATEER_BIN", ctx.getRaids())
 
 	f := false
 	if ctx.AllRaids == nil {
