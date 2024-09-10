b65529799821890987b791cf5cabed845a405abf
Eddie Knight <knight@linux.com>
2024-09-09 20:17:52 -0500

    Updated to use latest CCC output format
    Signed-off-by: Eddie Knight <knight@linux.com>

--- cmd/generate-raid.go
+++ cmd/generate-raid.go
@@ -9,13 +9,14 @@ import (
 	"strings"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/labstack/gommon/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"gopkg.in/yaml.v2"
 )
 
 // ComponentDefinition represents the structure of the input YAML file.
-type ComponentDefinition struct {
+type ComponentDefinitionOLD struct {
 	ServiceName        string
 	CategoryIDFriendly string
 	CategoryID         string    `yaml:"category-id"`
@@ -24,8 +25,7 @@ type ComponentDefinition struct {
 }
 
 // Control represents the structure of each control within the YAML file.
-type Control struct {
-	ServiceName      string
+type ControlOLD struct {
 	IDFriendly       string
 	ID               string              `yaml:"id"`
 	FeatureID        string              `yaml:"feature-id"`
@@ -37,7 +37,53 @@ type Control struct {
 	TestRequirements map[string]string   `yaml:"test-requirements"`
 }
 
-var Data ComponentDefinition
+type ComponentDefinition struct {
+	CategoryIDFriendly string
+	ServiceName        string
+	Metadata           Metadata  `yaml:"metadata"`
+	Controls           []Control `yaml:"controls"`
+	Features           []Feature `yaml:"features"`
+	Threats            []Threat  `yaml:"threats"`
+}
+
+type Control struct {
+	IDFriendly       string
+	ID               string                 `yaml:"id"`
+	Title            string                 `yaml:"title"`
+	Objective        string                 `yaml:"objective"`
+	ControlFamily    string                 `yaml:"control_family"`
+	Threats          []string               `yaml:"threats"`
+	NISTCSF          string                 `yaml:"nist_csf"`
+	MITREATTACK      string                 `yaml:"mitre_attack"`
+	ControlMappings  map[string]interface{} `yaml:"control_mappings"`
+	TestRequirements map[int]string         `yaml:"test_requirements"`
+}
+
+// Metadata is a struct that represents the metadata.yaml file
+type Metadata struct {
+	Title              string `yaml:"title"`
+	ID                 string `yaml:"id"`
+	Description        string `yaml:"description"`
+	AssuranceLevel     string `yaml:"assurance_level"`
+	ThreatModelAuthor  string `yaml:"threat_model_author"`
+	ThreatModelURL     string `yaml:"threat_model_url"`
+	RedTeam            string `yaml:"red_team"`
+	RedTeamExercizeURL string `yaml:"red_team_exercize_url"`
+}
+
+type Feature struct {
+	ID          string `yaml:"id"`
+	Title       string `yaml:"title"`
+	Description string `yaml:"description"`
+}
+type Threat struct {
+	ID          string   `yaml:"id"`
+	Title       string   `yaml:"title"`
+	Description string   `yaml:"description"`
+	Features    []string `yaml:"features"`
+	MITRE       []string `yaml:"mitre_attack"`
+}
+
 var TemplatesDir string
 var SourcePath string
 var OutputDir string
@@ -71,6 +117,16 @@ func generateRaid() {
 		logger.Error(err.Error())
 		return
 	}
+	data, err := readData()
+	if err != nil {
+		log.Error(err.Error())
+		return
+	}
+	data.ServiceName = viper.GetString("service-name")
+	if data.ServiceName == "" {
+		log.Error(fmt.Errorf("--service-name is required to generate a raid."))
+		return
+	}
 
 	err = filepath.Walk(TemplatesDir,
 		func(path string, info os.FileInfo, err error) error {
@@ -78,7 +134,7 @@ func generateRaid() {
 				return err
 			}
 			if !info.IsDir() {
-				err = generateFileFromTemplate(path, OutputDir)
+				err = generateFileFromTemplate(data, path, OutputDir)
 				if err != nil {
 					logger.Error(fmt.Sprintf("Failed while writing in dir '%s': %s", OutputDir, err))
 				}
@@ -107,25 +163,21 @@ func setupTemplatingEnvironment() error {
 	}
 
 	OutputDir = viper.GetString("output-dir")
-	logger.Trace("Generated raid will be stored in this directory: %s", OutputDir)
-
-	if viper.GetString("service-name") == "" {
-		return fmt.Errorf("--service-name is required to generate a raid.")
-	}
-	Data = readData()
-	Data.ServiceName = viper.GetString("service-name")
+	logger.Trace(fmt.Sprintf("Generated raid will be stored in this directory: %s", OutputDir))
 
 	return os.MkdirAll(OutputDir, os.ModePerm)
 }
 
 func setupTemplatesDir() error {
-	// Pull latest templates from git
+	// Remove any old templates
 	err := os.RemoveAll(TemplatesDir)
 	if err != nil {
 		logger.Error("Failed to remove templates directory: %s", err)
+
 	}
 
-	logger.Trace("Cloning templates repo to: ", TemplatesDir)
+	// Pull latest templates from git
+	logger.Trace(fmt.Sprintf("Cloning templates repo to: %s", TemplatesDir))
 	_, err = git.PlainClone(TemplatesDir, false, &git.CloneOptions{
 		URL:      "https://github.com/privateerproj/raid-generator-templates.git",
 		Progress: os.Stdout,
@@ -133,7 +185,7 @@ func setupTemplatesDir() error {
 	return err
 }
 
-func generateFileFromTemplate(templatePath, OutputDir string) error {
+func generateFileFromTemplate(data ComponentDefinition, templatePath, OutputDir string) error {
 	tmpl, err := template.ParseFiles(templatePath)
 	if err != nil {
 		return fmt.Errorf("error parsing template file %s: %w", templatePath, err)
@@ -155,7 +207,7 @@ func generateFileFromTemplate(templatePath, OutputDir string) error {
 	}
 	defer outputFile.Close()
 
-	err = tmpl.Execute(outputFile, Data)
+	err = tmpl.Execute(outputFile, data)
 	if err != nil {
 		return fmt.Errorf("error executing template for file %s: %w", outputPath, err)
 	}
@@ -163,52 +215,58 @@ func generateFileFromTemplate(templatePath, OutputDir string) error {
 	return nil
 }
 
-func readData() ComponentDefinition {
-	var Data ComponentDefinition
+func readData() (data ComponentDefinition, err error) {
 	if strings.HasPrefix(SourcePath, "http") {
-		Data = readYAMLURL()
+		data, err = readYAMLURL()
 	} else {
-		Data = readYAMLFile()
+		data, err = readYAMLFile()
 	}
-	Data.CategoryIDFriendly = strings.ReplaceAll(Data.CategoryID, ".", "_")
-	for i := range Data.Controls {
-		Data.Controls[i].IDFriendly = strings.ReplaceAll(Data.Controls[i].ID, ".", "_")
+	if err != nil {
+		return
 	}
-	return Data
+
+	data.CategoryIDFriendly = strings.ReplaceAll(data.Metadata.ID, ".", "_")
+	for i := range data.Controls {
+		data.Controls[i].IDFriendly = strings.ReplaceAll(data.Controls[i].ID, ".", "_")
+	}
+	return
 }
 
-func readYAMLURL() ComponentDefinition {
+func readYAMLURL() (data ComponentDefinition, err error) {
 	resp, err := http.Get(SourcePath)
 	if err != nil {
 		logger.Error("Failed to fetch URL: %v", err)
+		return
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		logger.Error("Failed to fetch URL: %v", resp.Status)
+		return
 	}
 
-	var Data ComponentDefinition
 	decoder := yaml.NewDecoder(resp.Body)
-	err = decoder.Decode(&Data)
+	err = decoder.Decode(&data)
 	if err != nil {
 		logger.Error("Failed to decode YAML from URL: %v", err)
+		return
 	}
 
-	return Data
+	return
 }
 
-func readYAMLFile() ComponentDefinition {
+func readYAMLFile() (data ComponentDefinition, err error) {
 	yamlFile, err := os.ReadFile(SourcePath)
 	if err != nil {
-		logger.Error("Error reading local source file: %s (%v)", SourcePath, err)
+		logger.Error(fmt.Sprintf("Error reading local source file: %s (%v)", SourcePath, err))
+		return
 	}
 
-	var Data ComponentDefinition
-	err = yaml.Unmarshal(yamlFile, &Data)
+	err = yaml.Unmarshal(yamlFile, &data)
 	if err != nil {
 		logger.Error("Error unmarshalling YAML file: %v", err)
+		return
 	}
 
-	return Data
+	return
 }
