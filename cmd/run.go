5ce9e852e289e1602046d959b54cf7989bcabe99
Zohayb Bhatti <zobi@revanite.io>
2025-10-28 00:09:16 -0500

    feat: Use SDK run command
    - Replace Run() implementation with command.Run()
    - Remove closeClient(), newClient() helper functions (moved to SDK)
    - Keep setupCloseHandler() for SIGTERM handling (exits process)
    - Re-export exit codes from SDK for backwards compatibility
    - This reduces code duplication between OSS and Enterprise

--- cmd/run.go
+++ cmd/run.go
@@ -3,15 +3,12 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"os/signal"
 	"syscall"
 
-	hcplugin "github.com/hashicorp/go-plugin"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 
-	"github.com/privateerproj/privateer-sdk/shared"
+	"github.com/privateerproj/privateer-sdk/command"
 )
 
 // runCmd represents the run command
@@ -32,13 +29,14 @@ When everything is battoned down, it is time to run forth.`,
 	},
 }
 
+// Re-export exit codes from SDK for backwards compatibility
 const (
-	TestPass = iota
-	TestFail
-	Aborted
-	InternalError
-	BadUsage
-	NoTests
+	TestPass      = command.TestPass
+	TestFail      = command.TestFail
+	Aborted       = command.Aborted
+	InternalError = command.InternalError
+	BadUsage      = command.BadUsage
+	NoTests       = command.NoTests
 )
 
 func init() {
@@ -47,73 +45,10 @@ func init() {
 
 // Run executes all plugins with handling for the command line
 func Run() (exitCode int) {
-
 	// Setup for handling SIGTERM (Ctrl+C)
 	setupCloseHandler()
-	logger.Trace(fmt.Sprintf(
-		"Using bin: %s", viper.GetString("binaries-path")))
-
-	plugins := GetPlugins()
-	if len(plugins) == 0 {
-		logger.Error(fmt.Sprintf("no plugins were requested in config: %s", viper.GetString("binaries-path")))
-		return NoTests
-	}
-
-	// Run all plugins
-	var runCount int
-	for serviceName := range viper.GetStringMap("services") {
-		servicePluginName := viper.GetString(fmt.Sprintf("services.%s.plugin", serviceName))
-		for _, pluginPkg := range plugins {
-			if pluginPkg.Name == servicePluginName {
-				if !pluginPkg.Available {
-					logger.Error(fmt.Sprintf("requested plugin that is not installed: %s", pluginPkg.Name))
-					return BadUsage
-				}
-				runCount++
-				client := newClient(pluginPkg.Command)
-				// Connect via RPC
-				var rpcClient hcplugin.ClientProtocol
-				rpcClient, err := client.Client()
-				if err != nil {
-					logger.Error(fmt.Sprintf("internal error while initializing %s RPC client: %s", serviceName, err))
-					closeClient(pluginPkg, serviceName, client)
-					return InternalError
-				}
-				// Request the plugin
-				var rawPlugin interface{}
-				rawPlugin, err = rpcClient.Dispense(shared.PluginName)
-				if err != nil {
-					logger.Error(fmt.Sprintf("internal error while dispensing RPC client: %s", err.Error()))
-					closeClient(pluginPkg, serviceName, client)
-					return InternalError
-				}
-				// Execute plugin
-				plugin := rawPlugin.(shared.Pluginer)
-				logger.Trace(fmt.Sprintf("Starting Plugin %v: %s", runCount, pluginPkg.Name))
-				response := plugin.Start()
-				if response != nil {
-					pluginPkg.Error = fmt.Errorf("tests failed in plugin %s: %v", serviceName, response)
-					exitCode = TestFail
-				} else {
-					pluginPkg.Successful = true
-				}
-				closeClient(pluginPkg, serviceName, client)
-			}
-		}
-	}
-	return exitCode
-}
-
-func closeClient(pluginPkg *PluginPkg, serviceName string, client *hcplugin.Client) {
-	// Close the client: this doesn't work via defer because it leaves the plugin running while the next begins
-	if pluginPkg.Successful {
-		logger.Info(fmt.Sprintf("Plugin for %s completed successfully", serviceName))
-	} else if pluginPkg.Error != nil {
-		logger.Error(pluginPkg.Error.Error())
-	} else {
-		logger.Error(fmt.Sprintf("unexpected exit while attempting to run package: %v", pluginPkg))
-	}
-	client.Kill()
+	
+	return command.Run(logger, GetPlugins)
 }
 
 // setupCloseHandler creates a 'listener' on a new goroutine which will notify the
@@ -128,20 +63,4 @@ func setupCloseHandler() {
 		logger.Error("Test execution was aborted by user")
 		os.Exit(int(Aborted))
 	}()
-}
-
-// newClient client handles the lifecycle of a plugin application
-// Plugin hosts should use one Client for each plugin executable
-// (this is different from the client that manages gRPC)
-func newClient(cmd *exec.Cmd) *hcplugin.Client {
-	var pluginMap = map[string]hcplugin.Plugin{
-		shared.PluginName: &shared.Plugin{},
-	}
-	var handshakeConfig = shared.GetHandshakeConfig()
-	return hcplugin.NewClient(&hcplugin.ClientConfig{
-		HandshakeConfig: handshakeConfig,
-		Plugins:         pluginMap,
-		Cmd:             cmd,
-		Logger:          logger,
-	})
 }
