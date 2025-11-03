9f9ec1b2d780e51b459c5eac2a409bc26f60a658
Zohayb Bhatti <zobi@revanite.io>
2025-11-03 14:56:35 -0600

    fix: Initialize writer at runtime for list command
    The writer variable is nil at package init time, causing a nil pointer
    error when the list command tries to use writer.Flush(). This fix creates
    a wrapper command that defers getting the SDK command until runtime,
    when writer has been initialized by persistentPreRun().
    
    Fixes the issue where list command was failing when using writer.

--- cmd/list.go
+++ cmd/list.go
@@ -1,14 +1,34 @@
 package cmd
 
 import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
 	"github.com/privateerproj/privateer-sdk/command"
 )
 
-
-// listCmd represents the list command
-var listCmd = command.GetListCmd(writer)
+var listCmd *cobra.Command
 
 func init() {
+	// Create a wrapper command that gets the writer at runtime
+	// This is necessary because writer is nil at package init time
+	// and only gets initialized in persistentPreRun()
+	listCmd = &cobra.Command{
+		Use:   "list",
+		Short: "Consult the Charts! List all plugins that have been installed.",
+		Run: func(cmd *cobra.Command, args []string) {
+			// Get the list command from SDK with the initialized writer
+			// At this point, writer has been initialized by persistentPreRun()
+			sdkListCmd := command.GetListCmd(writer)
+			// Execute the SDK command's Run function
+			sdkListCmd.Run(cmd, args)
+		},
+	}
+
+	// Add flags (matching the SDK command)
+	listCmd.PersistentFlags().BoolP("all", "a", false, "Review the Fleet! List all plugins that have been installed or requested in the current config")
+	_ = viper.BindPFlag("all", listCmd.PersistentFlags().Lookup("all"))
+
 	rootCmd.AddCommand(listCmd)
 }
 
