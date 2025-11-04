5b395daa6dfb056b1eecc5de966235b039b3150c
Zohayb Bhatti <97380019+zohayb23@users.noreply.github.com>
2025-11-04 13:44:54 -0600

    feat: Use SDK list command (#140)
    * feat: Use SDK list command
    
    - Replace local listCmd with SDK GetListCmd()
    - Remove duplicate plugin discovery functions (moved to SDK)
    - Re-export GetPlugins() and Contains() for backwards compatibility with other commands
    - This reduces code duplication between OSS and Enterprise
    
    * Update cmd/list.go
    
    * fix: Initialize writer at runtime for list command
    
    The writer variable is nil at package init time, causing a nil pointer
    error when the list command tries to use writer.Flush(). This fix creates
    a wrapper command that defers getting the SDK command until runtime,
    when writer has been initialized by persistentPreRun().
    
    Fixes the issue where list command was failing when using writer.
    
    ---------
    
    Co-authored-by: Eddie Knight <knight@linux.com>

--- cmd/list.go
+++ cmd/list.go
@@ -1,105 +1,37 @@
 package cmd
 
 import (
-	"fmt"
-	"log"
-	"path"
-	"strings"
-
-	hcplugin "github.com/hashicorp/go-plugin"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+
+	"github.com/privateerproj/privateer-sdk/command"
 )
 
-var (
-	cmdName = "list"
-)
-
-// listCmd represents the list command
-var listCmd = &cobra.Command{
-	Use:   cmdName,
-	Short: "Consult the Charts! List all plugins that have been installed.",
-	Run: func(cmd *cobra.Command, args []string) {
-		if viper.GetBool("all") {
-			_, _ = fmt.Fprintln(writer, "| Plugin \t | Available \t| Requested \t|")
-			for _, pluginPkg := range GetPlugins() {
-				_, _ = fmt.Fprintf(writer, "| %s \t | %t \t| %t \t|\n", pluginPkg.Name, pluginPkg.Available, pluginPkg.Requested)
-			}
-		} else {
-			// list only the available plugins
-			_, _ = fmt.Fprintln(writer, "| Plugin \t | Requested \t|")
-			for _, pluginPkg := range GetPlugins() {
-				if pluginPkg.Available {
-					_, _ = fmt.Fprintf(writer, "| %s \t | %t \t|\n", pluginPkg.Name, pluginPkg.Requested)
-				}
-			}
-		}
-		err := writer.Flush()
-		if err != nil {
-			log.Printf("Error flushing writer: %v", err)
-		}
-	},
-}
+var listCmd *cobra.Command
 
 func init() {
-	rootCmd.AddCommand(listCmd)
+	// Create a wrapper command that gets the writer at runtime
+	// This is necessary because writer is nil at package init time
+	// and only gets initialized in persistentPreRun()
+	listCmd = &cobra.Command{
+		Use:   "list",
+		Short: "Consult the Charts! List all plugins that have been installed.",
+		Run: func(cmd *cobra.Command, args []string) {
+			// Get the list command from SDK with the initialized writer
+			// At this point, writer has been initialized by persistentPreRun()
+			sdkListCmd := command.GetListCmd(writer)
+			// Execute the SDK command's Run function
+			sdkListCmd.Run(cmd, args)
+		},
+	}
 
+	// Add flags (matching the SDK command)
 	listCmd.PersistentFlags().BoolP("all", "a", false, "Review the Fleet! List all plugins that have been installed or requested in the current config")
 	_ = viper.BindPFlag("all", listCmd.PersistentFlags().Lookup("all"))
+
+	rootCmd.AddCommand(listCmd)
 }
 
-// GetRequestedPlugins returns a list of plugin names requested in the config
-func getRequestedPlugins() (requestedPluginPackages []*PluginPkg) {
-	services := viper.GetStringMap("services")
-	for serviceName := range services {
-		pluginName := viper.GetString("services." + serviceName + ".plugin")
-		pluginPkg := NewPluginPkg(pluginName, serviceName)
-		pluginPkg.Requested = true
-		requestedPluginPackages = append(requestedPluginPackages, pluginPkg)
-	}
-	return requestedPluginPackages
-}
-
-// GetAvailablePlugins returns a list of plugins found in the binaries path
-func getAvailablePlugins() (availablePluginPackages []*PluginPkg) {
-	pluginPaths, _ := hcplugin.Discover("*", viper.GetString("binaries-path"))
-	for _, pluginPath := range pluginPaths {
-		pluginPkg := NewPluginPkg(path.Base(pluginPath), "")
-		pluginPkg.Available = true
-		if strings.Contains(pluginPkg.Name, "privateer") {
-			continue
-		}
-		availablePluginPackages = append(availablePluginPackages, pluginPkg)
-	}
-	return availablePluginPackages
-}
-
-var allPlugins []*PluginPkg
-
-func GetPlugins() []*PluginPkg {
-	if allPlugins != nil {
-		return allPlugins
-	}
-	output := make([]*PluginPkg, 0)
-	for _, plugin := range getRequestedPlugins() {
-		if Contains(getAvailablePlugins(), plugin.Name) {
-			plugin.Available = true
-		}
-		output = append(output, plugin)
-	}
-	for _, plugin := range getAvailablePlugins() {
-		if !Contains(output, plugin.Name) {
-			output = append(output, plugin)
-		}
-	}
-	return output
-}
-
-func Contains(slice []*PluginPkg, search string) bool {
-	for _, plugin := range slice {
-		if plugin.Name == search {
-			return true
-		}
-	}
-	return false
-}
+// Re-export functions from SDK for backwards compatibility
+var GetPlugins = command.GetPlugins
+var Contains = command.Contains
