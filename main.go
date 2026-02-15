2b205ac6fb710eb54d43b35da7b61432c175ffc7
Eddie Knight <knight@linux.com>
2026-02-15 10:34:24 -0600

    chore: refactored entrypoint logic
    Signed-off-by: Eddie Knight <knight@linux.com>

--- main.go
+++ main.go
@@ -5,27 +5,51 @@ package main
 
 import (
 	"fmt"
+    "runtime/debug"
 
 	"github.com/privateerproj/privateer/cmd"
 )
 
 var (
 	// Version is the version string that will be replaced at build time by the associated tag.
-	Version = "0.0.0"
+	Version string
 	// VersionPostfix is a marker for the version such as "dev", "beta", "rc", etc.
 	// This is appended to the version string if it is not empty.
-	VersionPostfix = "dev"
+	VersionPostfix string
 	// GitCommitHash is the git commit hash at build time.
-	GitCommitHash = ""
+	GitCommitHash string
 	// BuiltAt is the actual build datetime string.
-	BuiltAt = ""
+	BuiltAt string
 )
+
+func setVCSInfo() {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+    }
+
+	var modified bool
+	for _, setting := range info.Settings {
+       switch setting.Key {
+       case "vcs.revision":
+          GitCommitHash = setting.Value
+       case "vcs.time":
+          BuiltAt = setting.Value
+       case "vcs.modified":
+          VersionPostfix = "-dev"
+       }
+    }
+}
 
 // main is the entry point for the Privateer application.
 // It formats the version string with any postfix and executes the root command.
 func main() {
+	if Version == "" {
+		Version = "edge"
+		setVCSInfo()
+	}
 	if VersionPostfix != "" {
-		Version = fmt.Sprintf("%s-%s", Version, VersionPostfix)
+		Version += VersionPostfix
 	}
 	cmd.Execute(Version, GitCommitHash, BuiltAt)
 }
