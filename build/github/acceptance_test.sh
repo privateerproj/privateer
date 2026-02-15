f81e291b8ea0ac79e361531b7506edc8b1bc8fca
jmeridth <jmeridth@gmail.com>
2026-02-15 10:13:28 -0600

    feat: add env command to display runtime environment info
    Closes https://github.com/privateerproj/privateer/issues/181
    
    ## What
    
    Add a new `env` command (aliased as `info`) that displays the binary
    path, config location, plugins directory, discovered plugins, version,
    commit, build time, Go version, and OS/arch.
    
    ## Why
    
    There was no way to quickly see which privateer binary is running or
    where it's looking for config and plugins, making troubleshooting
    harder than necessary.
    
    ## Notes
    
    - The `env` command overlaps with `version --verbose` for build info
      fields; this is intentional as they serve different purposes
    - Plugin discovery reuses the same hashicorp/go-plugin Discover call
      as the list command but filters out privateer binaries, matching
      existing behavior in the SDK
    - Config path comes from viper's "config" key which defaults to
      {cwd}/config.yml via the SDK's SetBase
    
    Signed-off-by: jmeridth <jmeridth@gmail.com>

--- build/github/acceptance_test.sh
+++ build/github/acceptance_test.sh
@@ -5,6 +5,7 @@ set -x
 STATUS=0
 
 ./privateer completion || STATUS=1
+./privateer env || STATUS=1
 ./privateer generate-plugin -p ./test/data/CCC.VPC_2025.01.yaml -n example || STATUS=1
 ./privateer help || STATUS=1
 ./privateer list || STATUS=1
