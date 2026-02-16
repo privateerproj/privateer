ca75317c0a1f70f5f15375bb771f9e6ae2bcf6e8
jmeridth <jmeridth@gmail.com>
2026-02-16 08:49:26 -0600

    fix: update acceptance test to use correct test data filename
    ## What
    
    Update the generate-plugin source path in the acceptance test from
    CCC.VPC_2025.01.yaml to OSPS_Baseline_AC_2025_02.yaml.
    
    ## Why
    
    The test data file was renamed but the acceptance test was not updated,
    causing the generate-plugin step to fail in CI.
    
    ## Notes
    
    - This was a pre-existing issue masked by the STATUS pattern allowing
      subsequent steps to continue despite the failure
    
    Signed-off-by: jmeridth <jmeridth@gmail.com>

--- build/github/acceptance_test.sh
+++ build/github/acceptance_test.sh
@@ -6,7 +6,7 @@ STATUS=0
 
 ./privateer completion || STATUS=1
 ./privateer env || STATUS=1
-./privateer generate-plugin -p ./test/data/CCC.VPC_2025.01.yaml -n example || STATUS=1
+./privateer generate-plugin -p ./test/data/OSPS_Baseline_AC_2025_02.yaml -n example || STATUS=1
 ./privateer help || STATUS=1
 ./privateer list || STATUS=1
 ./privateer run -b ./test/data/ || STATUS=1
