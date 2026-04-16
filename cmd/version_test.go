4dedfc05e93e049df14ef2388745155df71ebd4c
Satarupa Deb <84276219+Satarupa22-SD@users.noreply.github.com>
2026-04-17 03:54:38 +0530

    fix: test --verbose flag via SetArgs instead of viper.Set (#238)
    * update version test
    
    Signed-off-by: Satarupa22-SD <satarupa2212@gmail.com>
    
    * update go.mod
    
    Signed-off-by: Satarupa22-SD <satarupa2212@gmail.com>
    
    ---------
    
    Signed-off-by: Satarupa22-SD <satarupa2212@gmail.com>

--- cmd/version_test.go
+++ cmd/version_test.go
@@ -12,12 +12,12 @@ func TestVersionCmd_DefaultOutputShowsVersionOnly(t *testing.T) {
 	var buf bytes.Buffer
 	c := newTestCLI(&buf, "1.2.3", "a1b2c3d", "2026-01-01T00:00:00Z")
 	c.addVersionCmd()
-
-	viper.Set("verbose", false)
 	defer viper.Reset()
 
-	versionCmd, _, _ := c.rootCmd.Find([]string{"version"})
-	versionCmd.Run(versionCmd, []string{})
+	c.rootCmd.SetArgs([]string{"version"})
+	if err := c.rootCmd.Execute(); err != nil {
+		t.Fatalf("unexpected error executing version command: %v", err)
+	}
 
 	output := buf.String()
 
@@ -30,16 +30,16 @@ func TestVersionCmd_DefaultOutputShowsVersionOnly(t *testing.T) {
 	}
 }
 
-func TestVersionCmd_VerboseOutputShowsAllFields(t *testing.T) {
+func TestVersionCmd_VerboseFlagOutputShowsAllFields(t *testing.T) {
 	var buf bytes.Buffer
 	c := newTestCLI(&buf, "2.0.0", "f1e2d3c", "2026-06-15T12:00:00Z")
 	c.addVersionCmd()
-
-	viper.Set("verbose", true)
 	defer viper.Reset()
 
-	versionCmd, _, _ := c.rootCmd.Find([]string{"version"})
-	versionCmd.Run(versionCmd, []string{})
+	c.rootCmd.SetArgs([]string{"version", "--verbose"})
+	if err := c.rootCmd.Execute(); err != nil {
+		t.Fatalf("unexpected error executing version --verbose: %v", err)
+	}
 
 	output := buf.String()
 
