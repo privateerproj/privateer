3c8d139a413ce0ba2a1721036dbb6aba66a15d48
Satarupa22-SD <satarupa2212@gmail.com>
2026-05-24 01:52:26 +0530

    drop viper dependency
    Signed-off-by: Satarupa22-SD <satarupa2212@gmail.com>

--- cmd/version.go
+++ cmd/version.go
@@ -5,7 +5,6 @@ import (
 	"log"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 func (c *CLI) addVersionCmd() {
@@ -14,12 +13,15 @@ func (c *CLI) addVersionCmd() {
 		Short: "Display version details.",
 		Long:  `Display the version, git commit hash, and build timestamp of this pvtr build. Use the --verbose flag to see all details.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			if viper.GetBool("verbose") {
+			verbose, err := cmd.Flags().GetBool("verbose")
+			if err != nil {
+				log.Printf("Error reading verbose flag: %v", err)
+			}
+			if verbose {
 				_, _ = fmt.Fprintf(c.writer, "Version:\t%s\n", c.buildVersion)
 				_, _ = fmt.Fprintf(c.writer, "Commit:\t%s\n", c.buildGitCommitHash)
 				_, _ = fmt.Fprintf(c.writer, "Build Time:\t%s\n", c.buildTime)
-				err := c.writer.Flush()
-				if err != nil {
+				if err := c.writer.Flush(); err != nil {
 					log.Printf("Error flushing writer: %v", err)
 				}
 			} else {
@@ -32,7 +34,6 @@ func (c *CLI) addVersionCmd() {
 	}
 
 	versionCmd.Flags().BoolP("verbose", "v", false, "Display full version details including commit and build time")
-	_ = viper.BindPFlag("verbose", versionCmd.Flags().Lookup("verbose"))
 
 	c.rootCmd.AddCommand(versionCmd)
 }
